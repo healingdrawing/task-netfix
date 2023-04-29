d81db1dac0894a5133c1cdbd37296bd8766ddf4d
healingdrawing <healingdrawing@gmail.com>
2023-04-29 03:53:37 +0300

    Implement register pages

--- netfix/base/urls.py
+++ netfix/base/urls.py
@@ -4,4 +4,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('register/', views.register, name="register"),
+    path('register_customer/', views.register_customer, name="register_customer"),
+    path('register_company/', views.register_company, name="register_company"),
 ]
--- netfix/base/views.py
+++ netfix/base/views.py
@@ -5,3 +5,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+
+def register(request):
+    return render(request, 'register.html')
+
+
+def register_customer(request):
+    return render(request, 'register_customer.html')
+
+
+def register_company(request):
+    return render(request, 'register_company.html')
